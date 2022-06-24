ca1f57561a8acbedddd2d4b5e1843ad5cd3202ff
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-06-23 23:33:22 -0700

    Update stereogram.py

--- stereogram.py
+++ stereogram.py
@@ -40,8 +40,8 @@ for i in range(n):
   for j in range(n):
     if noise_og[i, j] != 666:
         bg_og[i, j, :] = 0
-    elif i > side_len && j > side_len:
-      if i < side_len*2 && j < side_len*2:
+    elif i > side_len and j > side_len:
+      if i < side_len*2 and j < side_len*2:
         bg_og[i, j, :] = 255
 
 noise_tmp = np.copy(noise)
