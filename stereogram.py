b5c9cd6f4f80d210e683c9313344ae615a52deee
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-06-23 22:10:06 -0700

    Update stereogram.py

--- stereogram.py
+++ stereogram.py
@@ -16,6 +16,15 @@ n_min = noise.min()
 n_max = noise.max()
 n_avg = (n_max + n_min) / 2
 
+side_len = np.shape(noise)[0] // 3
+
+left_offset = 1
+
+for i in range(side_len):
+  for j in range(side_len):
+    noise[side_len + i - 1, side_len + j] = noise[side_len + i, side_len + j]
+  noise[side_len + i - 1, side_len + side_len - 1] = 200 # np.random.normal(loc=0, scale=1, size=(1))
+
 noise_og = np.copy(noise)
 noise[noise > n_avg] = n_max
 noise[noise_og < n_avg] = n_min
