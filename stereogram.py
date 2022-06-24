be0a08833e52ec4953f4adb55917d6f01db02a5a
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-06-23 22:26:22 -0700

    Update stereogram.py

--- stereogram.py
+++ stereogram.py
@@ -35,7 +35,9 @@ noise[noise > n_avg] = n_max
 noise[noise_tmp < n_avg] = n_min
 
 #plt.imshow(noise_og, cmap='gray', vmin=n_min, vmax=n_max)
-plt.imshow(np.hstack((noise_og, noise)), cmap='gray', vmin=n_min, vmax=n_max)
+spacer = np.zeros((np.shape(noise)[0], ))
+print(np.shape(noise));exit()
+plt.imshow(np.hstack((noise_og, spacer, noise)), cmap='gray', vmin=n_min, vmax=n_max)
 plt.show()
 #plt.imshow(noise, cmap='gray', vmin=n_min, vmax=n_max)
 #plt.show()
