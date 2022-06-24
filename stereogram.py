b2b20e1fc971969e7f96942622002f814364d30c
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-06-23 21:32:09 -0700

    Update stereogram.py

--- stereogram.py
+++ stereogram.py
@@ -29,6 +29,8 @@ n4 = np.clip(np.where(img2 <= 1, (img2*(1 + noise*0.4)), (1-img2+1)*(1 + noise*0
 # norm noise for viz only
 noise2 = (noise - noise.min())/(noise.max()-noise.min())
 plt.figure(figsize=(20,20))
+plt.imshow(noise[:800,:800,:])
+plt.show()
 plt.imshow(np.vstack((np.hstack((img, noise[800,800,:])),
                       np.hstack((noisy, noisy2)),
                       np.hstack((noisy2mul, noisy4mul)),
