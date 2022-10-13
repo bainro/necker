0256cdd452b07f92c0e2f529f2fa9cbcf220fc89
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-10-13 15:13:01 -0700

    Update depth.py

--- depth.py
+++ depth.py
@@ -5,7 +5,21 @@ from PIL import Image
 
 depth_map = np.zeros([270,270])
 
-# back face first. Ie painter's algo for depth
+# painter's algo for depth
+
+
+# Note: hard coded for 45 degree line!
+p1x = 25
+p1y = 25
+p2x = 10+offset
+width = 15
+# draw a line of 1 px width between two points
+dx = abs(p1x - p2x)
+
+for i in range(dx + 15):
+  color = round(255 - i / (dx + 15) * (255-100))
+  depth_map[175+i,10+i:10+i+width] = color
+
 # top-left corner shold be around the middle of the first
 # @TODO use python's builtin slice() to make fx of cube
 # face involving top-left corner, line width, and edge length.
@@ -29,22 +43,10 @@ depth_map[10:175,160:175] = 255
 # left side
 depth_map[10:25,10:175] = 255
 
-# Note: hard coded for 45 degree line!
-p1x = 25
-p1y = 25
-p2x = 10+offset
-width = 15
-# draw a line of 1 px width between two points
-dx = abs(p1x - p2x)
-
 for i in range(dx):
   color = round(255 - i / dx * (255-100))
   depth_map[p1x+i:p1x+i+width,p1y+i] = color
   depth_map[p1x+i,p1y+i:p1y+i+width] = color
-
-for i in range(dx + 15):
-  # depth_map[p1x+i+145:p1x+i+width+145,p1y+i] = 170#color
-  depth_map[175+i,10+i:10+i+width] = 170#color
 
 # at each new pixel, extend line horizontally
 
