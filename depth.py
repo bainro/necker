701bef9c2fb314ef4f8be8ca3d2836bbe5e4770d
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-10-13 15:27:13 -0700

    Update depth.py

--- depth.py
+++ depth.py
@@ -20,7 +20,7 @@ dx = abs(p1x - p2x)
 
 for i in range(dx + 15):
   color = round(255 - i / dx * (255-100))
-  color = min(color, 100)
+  color = max(color, 100)
   depth_map[175+i,10+i:10+i+23] = color
 
 # top line
