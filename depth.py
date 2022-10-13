74b286a4450dc50e250235fec5ace73b45b06e4a
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-10-13 11:37:44 -0700

    Create depth.py

---
+++ depth.py
@@ -0,0 +1,17 @@
+# make int8 depth map & save as single channel
+import numpy as np
+import PIL
+
+depth_map = np.zeros([350,350])
+# draw explicit front face
+# top line
+depth_map[10:175,10:25] = 255
+# right side
+depth_map[160:175,10:175] = 255
+# bottom side
+depth_map[10:175,160:175] = 255
+# left side
+depth_map[10:25,10:175] = 255
+
+depth_png = PIL.Image.fromarray(depth_map)
+depth_png.save("cube.png", "PNG")
