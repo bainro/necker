5790d4369dc082a053d76bcb9915b8792cd28681
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-10-13 14:37:37 -0700

    Update depth.py

--- depth.py
+++ depth.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 from PIL import Image
 
-depth_map = np.zeros([185,185])
+depth_map = np.zeros([270,270])
 
 # back face first. Ie painter's algo for depth
 # top-left corner shold be around the middle of the first
