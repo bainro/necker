4ed80b68c56bf66d913130e486b491721617fc9b
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-10-13 12:04:34 -0700

    Update depth.py

--- depth.py
+++ depth.py
@@ -28,6 +28,10 @@ depth_map[10:175,160:175] = 255
 # left side
 depth_map[10:25,10:175] = 255
 
+# https://github.com/npinto/opencv/blob/master/modules/core/src/drawing.cpp#L237
+# https://github.com/npinto/opencv/blob/master/modules/core/src/drawing.cpp#L151
+
+
 depth_png = Image.fromarray(depth_map)
 depth_png = depth_png.convert("L")
 depth_png.save("cube.png", "PNG")
