3decaf77e5f5694462e006742351a7e111eac97a
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-10-13 11:47:00 -0700

    Update depth.py

--- depth.py
+++ depth.py
@@ -13,6 +13,18 @@ depth_map[10:175,160:175] = 255
 # left side
 depth_map[10:25,10:175] = 255
 
+# back face
+# top-left corner shold be around the middle of the first
+offset = 83
+# top line
+depth_map[10+offset:175+offset,10+offset:25+offset] = 100
+# right side
+depth_map[160+offset:175+offset,10+offset:175+offset] = 100
+# bottom side
+depth_map[10+offset:175+offset,160+offset:175+offset] = 100
+# left side
+depth_map[10+offset:25+offset,10+offset:175+offset] = 100
+
 depth_png = Image.fromarray(depth_map)
 depth_png = depth_png.convert("L")
 depth_png.save("cube.png", "PNG")
